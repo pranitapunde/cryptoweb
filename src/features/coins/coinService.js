fff6059acf7287b124d7063a08f4c65021945b4f
pranitapunde <pundepranita5@gmail.com>
2024-03-20 15:45:32 +0530

    Second Commit

--- src/features/coins/coinService.js
+++ src/features/coins/coinService.js
@@ -1,19 +1,19 @@
 import axios from 'axios'
 
 export const fetchCoins = async () => {
-    const response = await axios.get('/api/v3/search/trending')
+    const response = await axios.get('https://api.coingecko.com/api/v3/search/trending')
     console.log(response.data.coins)
     return response.data.coins;
 }
 
 export const fetchAllCoins = async () => {
-    const response = await axios.get("/api/v3//coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en");
+    const response = await axios.get("https://api.coingecko.com/api/v3//coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en");
     // console.log(response.data);
     return response.data;
 }
 
 export const fetchSearchCoins = async(name) => {
-    const response = await axios.get(`/api/v3/search?query=${name}`);
+    const response = await axios.get(`https://api.coingecko.com/api/v3/search?query=${name}`);
     // console.log(response.data.coins);
     return response.data.coins;
 } 
